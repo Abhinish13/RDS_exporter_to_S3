959f2f5f7d165f2210b002adf2d478f6a7f51694
Abhinish Raj <abhinish.gecr@gmail.com>
2020-02-12 14:22:18 +0530

    Time format changed #1

--- cloudwatchLogs_exporter.py
+++ cloudwatchLogs_exporter.py
@@ -44,11 +44,12 @@ def exporter(access,secret):
     print(deletionDate)
     # For deletionDate : replacing the time for deleting the cloudwatch
     # Start of Day: Begining of the day i.e 00:00:00:0000 Time at DeletionDate
-    startOfDay = deletionDate.replace(
-        hour=0, minute=0, second=0, microsecond=0)
+    unix_start = datetime(1970, 1, 1)
+    startOfDay = datetime.combine(date.today(), time()).replace(
+    hour=00, minute=00, second=00, microsecond=000000) - unix_start
     # End of Dat : Ending of the day i.e. 23:59:59:9999999 Time at DeletionDate
-    endOfDay = deletionDate.replace(
-        hour=23, minute=59, second=59, microsecond=999999)
+    endOfDay = datetime.combine(date.today(), time()).replace(
+    hour=23, minute=59, second=59, microsecond=999999) - unix_start
     # Log group of AWS resource which will be deleted is stored to variable group_name
     # It a list to delete multiple Logs group
     logGroupName = "/aws/rds/instance/perf-masterdb/audit"
@@ -57,11 +58,13 @@ def exporter(access,secret):
     destinationS3Prefix = "db_logs"
     client = boto3.client("logs", region_name=region,aws_access_key_id=access, 
                       aws_secret_access_key=secret)
+      dt = datetime.now()
+      timestamp = time.mktime(dt.timetuple()) + dt.microsecond/1e6
     response = client.create_export_task(
         taskName="Export_CloudwatchLogs_{}".format(datetime.now()),
         logGroupName=logGroupName,
-        fromTime=math.floor(startOfDay.timestamp() * 1000),
-        to=math.floor(endOfDay.timestamp() * 1000),
+        fromTime = int((startOfDay).total_seconds() * 1000)
+        to = int((endOfDay).total_seconds() * 1000)
         destination=destinationS3,
         destinationPrefix=destinationS3Prefix,
     )
