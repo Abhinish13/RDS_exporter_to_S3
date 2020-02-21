54b5906e867aa955e24396bcd702ea59f41ee9bd
Abhinish Raj <abhinish.gecr@gmail.com>
2020-02-21 11:23:21 +0530

    code formatted

--- cloudwatchLogs_exporter.py
+++ cloudwatchLogs_exporter.py
@@ -12,27 +12,27 @@ import math
 region = "us-east-2"
 
 
-
 def main(argv):
-   try:
-      opts, args = getopt.getopt(argv,"hs:a:",["secret=","access="])
-   except getopt.GetoptError:
-      
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print('Invalid Secret Key and Access Key')
-         sys.exit()
-      elif opt in ("-s", "--secret"):
-         secret = arg
-      elif opt in ("-a", "--access"):
-         access = arg
-   exporter(access,secret)
+    try:
+        opts, args = getopt.getopt(argv, "hs:a:", ["secret=", "access="])
+    except getopt.GetoptError:
+
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('Invalid Secret Key and Access Key')
+            sys.exit()
+        elif opt in ("-s", "--secret"):
+            secret = arg
+        elif opt in ("-a", "--access"):
+            access = arg
+    exporter(access, secret)
 #    print 'Input file is "', inputfile
 #    print 'Output file is "', outputfile
 
-def exporter(access,secret):
-   # yesterday = datetime.combine(date.today() - timedelta(1), time())
+
+def exporter(access, secret):
+    # yesterday = datetime.combine(date.today() - timedelta(1), time())
     #today = datetime.combine(date.today(), time())
     #unix_start = datetime(1970, 1, 1)
     # Defination of N i.e. How many day the logs will be there in the Cloudwatch
@@ -45,23 +45,23 @@ def exporter(access,secret):
     # Start of Day: Begining of the day i.e 00:00:00:0000 Time at DeletionDate
     unix_start = datetime(1970, 1, 1)
     startOfDay = datetime.combine(date.today(), time()).replace(
-    hour=00, minute=00, second=00, microsecond=000000) - unix_start
+        hour=00, minute=00, second=00, microsecond=000000) - unix_start
     # End of Dat : Ending of the day i.e. 23:59:59:9999999 Time at DeletionDate
     endOfDay = datetime.combine(date.today(), time()).replace(
-    hour=23, minute=59, second=59, microsecond=999999) - unix_start
+        hour=23, minute=59, second=59, microsecond=999999) - unix_start
     # Log group of AWS resource which will be deleted is stored to variable group_name
     # It a list to delete multiple Logs group
     logGroupName = "/aws/rds/instance/perf-masterdb/audit"
 
     destinationS3 = "cloudwatchlogs-db-audit"
     destinationS3Prefix = "db_logs"
-    client = boto3.client("logs", region_name=region,aws_access_key_id=access, 
-                      aws_secret_access_key=secret)
+    client = boto3.client("logs", region_name=region, aws_access_key_id=access,
+                          aws_secret_access_key=secret)
     response = client.create_export_task(
         taskName="Export_CloudwatchLogs_{}".format(datetime.now()),
         logGroupName=logGroupName,
-        fromTime = int((startOfDay).total_seconds() * 1000),
-        to = int((endOfDay).total_seconds() * 1000),
+        fromTime=int((startOfDay).total_seconds() * 1000),
+        to=int((endOfDay).total_seconds() * 1000),
         destination=destinationS3,
         destinationPrefix=destinationS3Prefix,
     )
@@ -75,6 +75,7 @@ def exporter(access,secret):
         # Function call for RetentionInDays(1) nDays = 1
         retentionInDays=retention_days(nDays),
     )
+
 
 def retention_days(n):
     retentionInDays = [
@@ -102,4 +103,4 @@ def retention_days(n):
 
 
 if __name__ == "__main__":
-       main(sys.argv[1:])
+    main(sys.argv[1:])
